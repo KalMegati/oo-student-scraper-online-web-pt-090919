a7183c9499b220c4bc4e80b608868cff104d79d0
Srikalyan Megati <kalmegati@gmail.com>
2019-11-14 18:25:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each { |key, value|
-      attr_accessor 
+      attr_accessor key
       self.send("#{key}=", value)
     }
     @@all << self
